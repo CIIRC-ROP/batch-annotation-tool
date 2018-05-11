59a7c28fe472abe752c2bdaa3ae627872b2e1ea5
Libor Wagner <libor.wagner@cvut.cz>
2018-05-11 10:14:21 +0200

    Add comments to json config

--- klikatko/config.py
+++ klikatko/config.py
@@ -4,6 +4,7 @@
 import json
 import logging
 import collections
+import re
 
 log = logging.getLogger("Config")
 
@@ -13,8 +14,26 @@ class Config(collections.UserDict):
         self.data = self.__shared_data
 
         if filename is not None:
-            with open(filename) as f:
-                self.data.update(json.load(f))
+            self.data.update(json_load_commented(filename))
 
         if len(self.data):
             log.warn("Empty configuration!")
+
+
+def json_load_commented(filename, **kvargs):
+    with open(filename) as f:
+        text = f.read()
+
+    # taken from commentjson (https://github.com/vaidik/commentjson)
+    regex = r'\s*(#|\/{2}).*$'
+    regex_inline = r'(:?(?:\s)*([A-Za-z\d\.{}]*)|((?<=\").*\"),?)(?:\s)*(((#|(\/{2})).*)|)$'
+    lines = text.split('\n')
+
+    for index, line in enumerate(lines):
+        if re.search(regex, line):
+            if re.search(r'^' + regex, line, re.IGNORECASE):
+                lines[index] = ""
+            elif re.search(regex_inline, line):
+                lines[index] = re.sub(regex_inline, r'\1', line)
+
+    return json.loads('\n'.join(lines), **kvargs)
