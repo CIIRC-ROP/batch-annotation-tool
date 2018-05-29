3b2bf79ba9ec32a95462c371297d72595e89e047
Libor Wagner <libor.wagner@cvut.cz>
2018-05-29 13:13:41 +0200

    Made package

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='klikatko',
+      version='0.1',
+      description='CIIRC B635 Annontation Tools',
+      author='Libor Wagner',
+      author_email='libor.wagner@cvut.cz',
+      packages=['klikatko'],
+      scripts=['bin/klikatko'],
+      install_requires=[
+          'pillow',
+          'docopt',
+          'numpy',
+          'pandas'
+      ],
+     )
